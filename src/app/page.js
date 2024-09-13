889a29c579766e5db0c480121a78f80fc11c72ce
Kartik Srivastava <kaushiksri0908@gmail.com>
2024-09-14 01:28:48 +0530

    Added Google Lens image upload box

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,21 @@
 "use client"
-
 import Button from "@/components/Button"
 import Footer from "@/components/Footer"
 import Header from "@/components/Header"
+import LensSearch from "@/components/LensSearch"
 import SearchBox from "@/components/SearchBox"
-import Image from "next/image"
-import React, { useRef, useState } from "react"
+import { useRef, useState } from "react"
 
 export default function Home() {
+  const [isLensSearch, setIsLensSearch] = useState(false)
   const [showTrending, setIsShowTrending] = useState(false)
   const lensSearchRef = useRef(null)
   const searchBoxRef = useRef(null)
 
   const handleContainerEvents = (e) => {
+    if (lensSearchRef.current && !lensSearchRef.current.contains(e.target)) {
+      setIsLensSearch(false)
+    }
     if (searchBoxRef.current && !searchBoxRef.current.contains(e.target)) {
       setIsShowTrending(false)
     }
@@ -23,47 +26,33 @@ export default function Home() {
       className="flex flex-col min-h-screen overflow-y-auto bg-[#202124]"
       onMouseDown={handleContainerEvents}
     >
-      {" "}
       <Header />
+
       <main
-        className={`flex flex-col  items-center justify-center flex-grow ${
-          showTrending ? "mb-56 pb-3 " : ""
-        }`}
+        className={`flex flex-col  items-center justify-center ${
+          isLensSearch ? "mt-20" : ""
+        } flex-grow ${showTrending ? "mb-40 pb-2.5" : ""}`}
       >
-        <Image
+        <img
           src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_272x92dp.png"
           alt="Logo"
-          width={272}
-          height={92}
+          className="mb-6 w-[272px]"
         />
-        <SearchBox
-          className="mt-6"
-          showTrending={showTrending}
-          setShowTrending={setIsShowTrending}
-          ref={searchBoxRef}
-        />
-
-        {!showTrending && (
-          <React.Fragment>
-            <div className="mt-8">
-              <Button text="Google Search" className="mr-3" />
-              <Button text="I'm Feeling Lucky" />
-            </div>
-            <div className="text-[12px] flex text-[#bfbfbf] font-bold mt-6 mb-32">
-              Google offered in:
-              <span className="ml-2 flex justify-between w-[380px]">
-                <span className="text-[#99c3ff]">हिन्दी</span>
-                <span className="text-[#99c3ff]">বাংলা</span>
-                <span className="text-[#99c3ff]">తెలుగు</span>
-                <span className="text-[#99c3ff]">मराठी</span>
-                <span className="text-[#99c3ff]">தமிழ்</span>
-                <span className="text-[#99c3ff]">ગુજરાતી</span>
-                <span className="text-[#99c3ff]">ಕನ್ನಡ</span>
-                <span className="text-[#99c3ff]">മലയാളം</span>
-                <span className="text-[#99c3ff]">ਪੰਜਾਬੀ</span>
-              </span>
-            </div>
-          </React.Fragment>
+        {isLensSearch ? (
+          <LensSearch ref={lensSearchRef} />
+        ) : (
+          <SearchBox
+            setIsLensSearch={setIsLensSearch}
+            showTrending={showTrending}
+            setShowTrending={setIsShowTrending}
+            ref={searchBoxRef}
+          />
+        )}
+        {isLensSearch ? null : (
+          <div className="mt-8">
+            <Button text="Google Search" className="mr-3" />
+            <Button text="I'm Feeling Lucky" />
+          </div>
         )}
       </main>
       <Footer />
---
+++ src/components/LensSearch.js
@@ -0,0 +1,76 @@
+import { forwardRef } from "react"
+import Close from "../../public/svgs/close.svg"
+import Placeholder from "../../public/svgs/placeholder.svg"
+
+const LensSearch = forwardRef((props, ref) => {
+  return (
+    <div
+      ref={ref}
+      className="relative w-full flex items-center justify-center h-[362px] max-w-[592px] bg-[#303134] mb-1 rounded-[25px]"
+    >
+      <div className="w-[555px]  h-full">
+        <span className="absolute right-0 translate-y-4 -translate-x-full">
+          <Close width={25} height={25} />
+        </span>
+
+        <div className="h-[60px] flex justify-center text-[#F1F3F4] items-center">
+          Search any image with Google Lens
+        </div>
+        <div className="bg-[#202125] w-full border-dashed border border-[#3c4043] h-[280px] justify-end flex flex-col items-center rounded-[10px]">
+          <div className="h-[200px] w-11/12 flex items-center justify-center ">
+            <span className="flex items-center justify-center gap-6">
+              <Placeholder w={100} /> Drag an image here or upload a file
+            </span>
+          </div>
+          <div className="h-[80px] w-11/12 border-gray-600 border-t-[0.5px] relative">
+            <span className="absolute flex items-center justify-center top-0 -translate-y-3.5 w-[60px] left-56 bg-[#202125]">
+              OR
+            </span>
+            <div className="h-full w-full flex items-center">
+              <input
+                type="text"
+                placeholder="Paste image link"
+                className="
+                  bg-[#303134] 
+                  border border-[rgb(60,64,67)] 
+                  hover:border-gray-500
+                  hover:broder-[0.5px]
+                  text-[rgb(241,243,244)] 
+                  rounded-[36px] 
+                  flex-grow 
+                  text-[14px] 
+                  h-[40px] 
+                  px-[24px] 
+                  w-full 
+                  outline-none
+                "
+              />
+              <button
+                className="
+                  flex items-center 
+                  bg-[#303134] 
+                  rounded-[32px] 
+                  border border-[rgb(60,64,67)]
+                  hover:text-[rgb(210,227,252)]
+                  hover:bg-[rgba(136,170,187,0.04)]
+                  hover:border-[rgb(60,64,67)]
+                  text-[rgb(138,180,248)] 
+                  cursor-pointer 
+                  justify-center 
+                  text-[14px] 
+                  ml-[8px] 
+                  outline-none 
+                  py-[8px] 
+                  px-[24px] 
+                "
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+})
+export default LensSearch
